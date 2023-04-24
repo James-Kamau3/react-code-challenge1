b626eb7e41411b143b839a2e2183435e280b0b30
James <kamaujames3705@gmail.com>
2023-04-24 21:05:29 +0300

    Added padding around contents

--- react-web/src/App.js
+++ react-web/src/App.js
@@ -7,17 +7,38 @@ function App() {
 
   // Add useEffect hook
   useEffect(() => {
-    fetch("http://localhost:8001/transactions")
+    fetch("http://localhost:3000/transactions")
       .then((r) => r.json())
-      .then((transactions) => setTransactions(transactions));
-  }, []);
+      .then((json) => setTransactions(json));
+  }, []);       
    
 return(
-<div>
-  {transactions.map((transactions) => {})}
-</div>
+<>
+<table className="App">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Date</th>
+          <th>Description</th>
+          <th>Category</th>
+          <th>Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transactions.map(transaction => (
+          <tr key={transaction.id}>
+            <td>{transaction.id}</td>
+            <td>{transaction.date}</td>
+            <td>{transaction.description}</td>
+            <td>{transaction.category}</td>
+            <td>{transaction.amount}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+
+</>
 );
 }
-
 
 export default App;
