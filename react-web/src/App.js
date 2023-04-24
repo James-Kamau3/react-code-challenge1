a79cef294e86a3957adc88edae352c9e33b21262
James <kamaujames3705@gmail.com>
2023-04-24 14:21:50 +0300

    Added Transactions component

--- react-web/src/App.js
+++ react-web/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [transactions,setTransactions] = useState([]);
+
+  // Add useEffect hook
+  useEffect(() => {
+    fetch("http://localhost:8001/transactions")
+      .then((r) => r.json())
+      .then((transactions) => setTransactions(transactions));
+  }, []);
+   
+return(
+<div>
+  {transactions.map((transactions) => {})}
+</div>
+);
 }
+
 
 export default App;
