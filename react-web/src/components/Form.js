fb73edd972a56eb30353e970afedc77ce96794a0
James <kamaujames3705@gmail.com>
2023-04-25 07:13:03 +0300

    Added Form component

--- react-web/src/components/Form.js
+++ react-web/src/components/Form.js
@@ -2,18 +2,7 @@ import React from 'react'
 
 function Form() {
   return (
-    <div>
-    <form>
-      <label>Date</label>
-      <input type="number"  value="yy/mm/dd"/>
-    <label >Description:</label>
-    <input type="text"/>
-    <label >Category:</label>
-    <input type="text"/>
-    <label >Amount:</label>
-    <input type="number"/>
-    </form>
-    </div>
+
   );
 }
 
